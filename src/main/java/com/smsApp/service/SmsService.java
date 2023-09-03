e4a6b534a1fbfd77eaba5c30e824e03acb0f65b8
kamal725528 <kamalsingh725528@gmail.com>
2023-09-03 13:55:08 +0530

    third party integration

---
+++ src/main/java/com/smsApp/service/SmsService.java
@@ -0,0 +1,33 @@
+package com.smsApp.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+    @Value("${twilio.accountSid}")
+    private String accountSid;
+
+    @Value("${twilio.authToken}")
+    private String authToken;
+
+    @Value("${twilio.phoneNumber}")
+    private String phoneNumber;
+
+    public void sendSms(String toPhoneNumber, String messageBody) {
+        Twilio.init(accountSid, authToken);
+
+        Message message = Message.creator(
+                        new PhoneNumber(toPhoneNumber),
+                        new PhoneNumber(phoneNumber),
+                        messageBody)
+                .create();
+
+        System.out.println("SMS Sent with SID: " + message.getSid());
+    }
+}
+
